55f342e9e3d47529bc429ba9f422a5b4a339bb54
Daniel Schrader <dhs@schrader-wolff-media.de>
2022-03-08 00:41:26 +0100

    Initial commit

---
+++ src/components/CounterButton.js
@@ -0,0 +1,11 @@
+import './CounterButton.css';
+
+export default function CounterButton() {
+  return (
+    <div className="CounterButton">
+      <button className="Number">20</button>
+      <button className="Double">x2</button>
+      <button className="Triple">x3</button>
+    </div>
+  );
+}
